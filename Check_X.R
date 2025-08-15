b1115c59fcd47d7541b5dd3e25839b530af36a13
Dinnersteak <Koeljor@gmail.com>
2025-08-15 15:00:43 -0500

    Add actual code to Check_X.R

---
+++ Check_X.R
@@ -0,0 +1,15 @@
+# Check_X.R
+
+check_x <- function(x) {
+    if (x > 47) {
+        print(":(")
+    } else if (x < 47) {
+        print(":(")
+    } else {
+        print(":)")
+    }
+}
+
+# Example usage
+check_x(47)
+check_x(67)
